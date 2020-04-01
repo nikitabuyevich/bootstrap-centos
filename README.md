f8104caffe82356984aeeb0f261f8b5d913bdea7
Nikita Buyevich <nikita.buyevich@gmail.com>
2020-03-31 22:28:45 -0500

    Release

---
+++ README.md
@@ -0,0 +1,68 @@
+![Logo of Centos](./centos.png)
+
+# Bootstrap / Harden CentOS &middot; [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)](./LICENSE)
+
+A bootstrapping / hardening CentOS bash script. It is meant to be used on new systems.
+
+The motivation for this script is to easily serve up web servers / microservices.
+
+## Features
+
+- Automatic yum updates
+- Idle users will be disconnected after 15 minutes
+- Secure SSH Daemon
+  - Set SSH to a random number
+  - Creates an `admin` user - Only they can login
+- Two-Factor Authentication (2FA)
+- Creates a swap file
+- Synchronizes a specified time zone
+- Password policies
+  - Passwords will expire every 180 days
+  - Passwords may only be changed once a day
+- Block common network attacks
+  - Syn Floods
+  - Fragmented Packets
+  - Malformed XMAS Packets
+  - Drop NULL packets
+  - Limit pings to 3 per second and bursts of 25
+  - Discourage Port Scanning
+- Connection tracking
+- Install Docker (optional)
+- Install Gitlab Runner (optional)
+- Register Gitlab Runner (optional)
+- Install DDoS Defalate - https://github.com/jgmdev/ddos-deflate
+- Install CHKROOTKIT - http://www.chkrootkit.org
+- Install Root Kit Hunter (rkhunter) - http://rkhunter.sourceforge.net
+- Install Linux Socket Monitor (LSM) - Runs in the background and watches for changes in sockets
+
+## Getting Started
+
+As root user, execute
+
+```bash
+ source <(curl -s https://raw.githubusercontent.com/nikitabuyevich/bootstrap-centos/master/bootstrap-centos.sh)
+```
+
+### Prerequisites
+
+- [CentOS](https://www.centos.org/) - Linux distribution that is a consistent, manageable platform that suits a wide variety of deployments
+
+### Configuration
+
+By default, the only TCP ports which are exposed are `HTTP` / `HTTPS` / `SSH`. Modify the `TCP_PORTS` variable if you wish to add more.
+
+## Built With
+
+- [Bash](https://www.gnu.org/software/bash/) - Unix shell and command language
+
+## Authors
+
+- **Nikita Buyevich** - [nikitabuyevich.com](http://nikitabuyevich.com/)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](./LICENSE) file for details.
+
+## Acknowledgments
+
+Forked and modified from https://www.limestonenetworks.com/support/knowledge-center/11/83/hardening_centos.html
---
+++ bootstrap-centos.sh
@@ -0,0 +1,533 @@
+#!/bin/bash
+# Created by Nikita Buyevich
+# Last updated on March 31st, 2020
+# Forked from https://www.limestonenetworks.com/support/knowledge-center/11/83/hardening_centos.html
+
+echo "-------------- Bootstrapping your CentOS --------------"
+echo "It is recommended that you only run this on a new system."
+echo "---------------------------------------------------------"
+cd /root
+
+# --- GLOBAL VARIABLES ---
+# Open HTTP / HTTPS
+TCP_PORTS=( 80 443 )
+
+
+
+echo "--- Create Admin User ---"
+echo "-------------------------"
+useradd -m admin
+passwd admin
+# Give admin sudo access
+gpasswd -a admin wheel
+
+
+
+echo "--- Bootstrap Setup ---"
+echo "-----------------------"
+# Set alert email address
+read -p "Enter an email address to be notified about alerts: " ALERT_EMAIL
+# Set SSH port
+read -p "Enter a new SSH port (or press enter for random): " SSH_PORT
+# Randomize SSH port if it wasn't provided
+if [[ "$SSH_PORT" == "" ]]
+then SSH_PORT=$((RANDOM%5000+2000))
+fi
+# Add ssh port to tcp ports to accept
+TCP_PORTS+=( $SSH_PORT )
+echo "--------------------------------------- Create a Swap File ----------------------------------------"
+echo "Advice about the best size for a swap space varies significantly depending on the source consulted."
+echo "Generally, an amount equal to or double the amount of RAM on your system is a good starting point."
+echo "---------------------------------------------------------------------------------------------------"
+read -p "Enter a swap file size (e.g., 4G): " SWAP_FILE_SIZE
+read -p "Enter a time zone (e.g., Europe/Berlin): " TIME_ZONE
+read -p "Install Docker [y/n]: " INSTALL_DOCKER
+read -p "Install Gitlab Runner [y/n]: " INSTALL_GITLAB_RUNNER
+case "$INSTALL_GITLAB_RUNNER" in
+      y|Y ) read -p "Register Gitlab Runner [y/n]: " REGISTER_GITLAB_RUNNER;;
+esac
+
+
+
+echo "--- Enable 2FA ---"
+echo "---------------------"
+# Install 2FA
+yum install -y epel-release
+yum install -y google-authenticator
+# Setup 2FA as admin user
+sudo -u admin -H sh -c "google-authenticator"
+
+# Update PAM file to include google-authenticator
+/bin/cat << EOM > /etc/pam.d/sshd
+#%PAM-1.0
+auth       required     pam_sepermit.so
+# auth       substack     password-auth
+auth       include      postlogin
+# Used with polkit to reauthorize users in remote sessions
+-auth      optional     pam_reauthorize.so prepare
+account    required     pam_nologin.so
+account    include      password-auth
+password   include      password-auth
+# pam_selinux.so close should be the first session rule
+session    required     pam_selinux.so close
+session    required     pam_loginuid.so
+# pam_selinux.so open should only be followed by sessions to be executed in the user context
+session    required     pam_selinux.so open env_params
+session    required     pam_namespace.so
+session    optional     pam_keyinit.so force revoke
+session    include      password-auth
+session    include      postlogin
+# Used with polkit to reauthorize users in remote sessions
+-session   optional     pam_reauthorize.so prepare
+auth required pam_google_authenticator.so
+EOM
+
+
+
+echo "--- Scheduling Auto Updates ---"
+echo "-------------------------------"
+# Turn on cron-based auto-updates
+yum install -y yum-cron
+for d in crond yum yum-cron; do
+    /sbin/chkconfig $d on
+    /sbin/service $d start
+done
+
+
+
+echo "--- Installing Useful Packages ---"
+echo "----------------------------------"
+# Installing useful packages
+yum -y install wget unzip
+
+
+
+echo "--- Insure All Packages Are Up To Date  ---"
+echo "-------------------------------------------"
+yum update -y
+
+
+
+echo "--- Creating Swap File ---"
+echo "--------------------------"
+# Create swapfile
+fallocate -l $SWAP_FILE_SIZE /swapfile
+# Restrict access to it
+chmod 600 /swapfile
+# Format file for swap
+mkswap /swapfile
+# Use the swapfile
+swapon /swapfile
+# Automatically use swapfile on reboot
+sh -c 'echo "/swapfile none swap sw 0 0" >> /etc/fstab'
+
+
+
+echo "--- Setting Time Zone ---"
+echo "---------------------"
+# Set time zone
+timedatectl set-timezone $TIME_ZONE
+# Install ntp
+yum install -y ntp
+# Start ntp
+systemctl enable ntpd
+systemctl start ntpd
+
+
+
+echo "--- Install git and git lfs ---"
+echo "-------------------------------"
+# Remove old git packages
+yum remove -y git*
+# Install git
+yum install -y  https://centos7.iuscommunity.org/ius-release.rpm
+yum install -y  git2u-all
+# Install git lfs
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | bash
+yum install -y git-lfs
+git lfs install
+
+
+
+case "$INSTALL_DOCKER" in
+      y|Y ) echo "Installing Docker...";
+            # Install Docker
+            yum install -y yum-utils device-mapper-persistent-data lvm2
+            yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+            yum install docker-ce docker-ce-cli containerd.io -y
+            # Run Docker
+            systemctl enable docker
+            systemctl start docker
+            # Install docker-compose
+            curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+            chmod +x /usr/local/bin/docker-compose
+            break;;
+esac
+
+
+
+case "$INSTALL_GITLAB_RUNNER" in
+      y|Y ) echo "Installing Gitlab Runner...";
+            # Install Gitlab Runner
+            curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.rpm.sh | bash
+            yum install -y gitlab-runner
+            break;;
+esac
+
+
+
+case "$REGISTER_GITLAB_RUNNER" in
+      y|Y ) echo "Registering Gitlab Runner...";
+            # Register Gitlab Runner
+            # See # See https://docs.gitlab.com/runner/register/index.html
+            gitlab-runner register
+            break;;
+esac
+
+
+
+echo "---                     Setting Password Policies                        ---"
+echo "--- Per recommendations from http://wiki.centos.org/HowTos/OS_Protection ---"
+echo "----------------------------------------------------------------------------"
+echo "Passwords will expire every 180 days"
+perl -npe 's/PASS_MAX_DAYS\s+99999/PASS_MAX_DAYS 180/' -i /etc/login.defs
+echo "Passwords may only be changed once a day"
+perl -npe 's/PASS_MIN_DAYS\s+0/PASS_MIN_DAYS 1/g' -i /etc/login.defs
+
+
+
+echo "---            Setting Additional OS Policies/Securities                 ---"
+echo "--- Per recommendations from http://wiki.centos.org/HowTos/OS_Protection ---"
+echo "----------------------------------------------------------------------------"
+# Now that we've restricted the login options for the server,
+# lets kick off all the idle folks. To do this,
+# we're going to use a bash variable in /etc/profile.
+# There are some reasonably trivial ways around this of course,
+# but it's all about layering the security.
+echo "Idle users will be removed after 15 minutes"
+echo "readonly TMOUT=900" >> /etc/profile.d/os-security.sh
+echo "readonly HISTFILE" >> /etc/profile.d/os-security.sh
+chmod +x /etc/profile.d/os-security.sh
+
+# In some cases, administrators may want the root user
+# or other trusted users to be able to run cronjobs
+# or timed scripts with at. In order to lock these down,
+# you will need to create a cron.deny and at.deny file inside /etc
+# with the names of all blocked users. An easy way to do this is
+# to parse /etc/passwd. The script below will do this for you.
+echo "Locking down Cron"
+touch /etc/cron.allow
+chmod 600 /etc/cron.allow
+awk -F: '{print $1}' /etc/passwd | grep -v root > /etc/cron.deny
+echo "Locking down AT"
+touch /etc/at.allow
+chmod 600 /etc/at.allow
+awk -F: '{print $1}' /etc/passwd | grep -v root > /etc/at.deny
+
+
+
+echo "--- Install and Clear IPTables Firewall  ---"
+echo "--------------------------------------------"
+yum install -y iptables
+chkconfig iptables on
+/sbin/service iptables start
+/sbin/iptables -F
+/sbin/iptables -X
+iptables-save
+
+
+echo "--- Running Firewall Configurations ---"
+echo "---------------------------------------"
+
+# By default reject all traffic
+/sbin/iptables -P INPUT DROP
+/sbin/iptables -P OUTPUT DROP
+/sbin/iptables -P FORWARD DROP
+
+# Allow localhost
+/sbin/iptables -A INPUT -i lo -j ACCEPT
+/sbin/iptables -A OUTPUT -o lo -j ACCEPT
+
+# Allow output for new, related and established connections
+/sbin/iptables -A OUTPUT -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
+
+# Open TCP Ports
+for port in ${TCP_PORTS[@]}
+    do
+        echo "Opening TCP Port $port"
+        /sbin/iptables -A INPUT -p tcp -m tcp --dport $port -j ACCEPT
+    done
+
+# Enable ntp
+iptables -A OUTPUT -p udp --dport 123 -j ACCEPT
+iptables -A INPUT -p udp --sport 123 -j ACCEPT
+
+
+echo "--- Blocking Common Attacks ---"
+echo "-------------------------------"
+
+echo "Forcing SYN packets check"
+/sbin/iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
+
+echo "Forcing Fragments packets check"
+/sbin/iptables -A INPUT -f -j DROP
+
+echo "Dropping malformed XMAS packets"
+/sbin/iptables -A INPUT -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
+
+echo "Drop all NULL packets"
+/sbin/iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+
+echo "Limiting pings to 1 per second"
+/sbin/iptables -N PACKET
+/sbin/iptables -A DEFAULT_RULES -p icmp -m limit --limit 3/sec --limit-burst 25 -j ACCEPT
+
+echo "Setup Connection Tracking"
+/sbin/iptables -N STATE_TRACK
+/sbin/iptables -A STATE_TRACK -m state --state RELATED,ESTABLISHED -j ACCEPT
+/sbin/iptables -A STATE_TRACK -m state --state INVALID -j DROP
+
+echo "Discouraging Port Scanning"
+/sbin/iptables -N PORTSCAN
+/sbin/iptables -A PORTSCAN -p tcp --tcp-flags ACK,FIN FIN -j DROP
+/sbin/iptables -A PORTSCAN -p tcp --tcp-flags ACK,PSH PSH -j DROP
+/sbin/iptables -A PORTSCAN -p tcp --tcp-flags ACK,URG URG -j DROP
+/sbin/iptables -A PORTSCAN -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
+/sbin/iptables -A PORTSCAN -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP
+/sbin/iptables -A PORTSCAN -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+/sbin/iptables -A PORTSCAN -p tcp --tcp-flags ALL ALL -j DROP
+/sbin/iptables -A PORTSCAN -p tcp --tcp-flags ALL NONE -j DROP
+/sbin/iptables -A PORTSCAN -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
+/sbin/iptables -A PORTSCAN -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP
+/sbin/iptables -A PORTSCAN -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
+
+
+echo "Setting Final Configurations"
+/sbin/iptables -N COMMON
+/sbin/iptables -A COMMON -j STATE_TRACK
+/sbin/iptables -A COMMON -j PORTSCAN
+/sbin/iptables -A COMMON -j PACKET
+
+/sbin/iptables -A INPUT -j COMMON
+/sbin/iptables -A OUTPUT -j COMMON
+/sbin/iptables -A FORWARD -j COMMON
+/sbin/iptables -A FORWARD -j PACKET
+
+echo "Saving IPTables"
+iptables-save
+
+
+
+echo "--- Installing DDoS Deflate ---"
+echo "-------------------------------"
+wget https://github.com/jgmdev/ddos-deflate/archive/master.zip -O ddos.zip
+unzip ddos.zip
+rm -rf ddos.zip
+yes | sh ddos-deflate-master/install.sh
+rm -rf ddos-deflate-master
+
+
+
+echo "--- Installing CHKROOTKIT  ---"
+echo "------------------------------"
+wget -O /usr/local/src/chkrootkit.tar.gz ftp://ftp.pangeia.com.br/pub/seg/pac/chkrootkit.tar.gz
+tar -C /usr/local/src/ -zxvf /usr/local/src/chkrootkit.tar.gz
+mkdir /usr/local/chkrootkit
+mv -f /usr/local/src/chkrootkit*/* /usr/local/chkrootkit
+cd /usr/local/chkrootkit
+make sense
+cd /root
+
+/bin/cat << EOM > /etc/cron.daily/chkrootkit.sh
+#!/bin/sh
+(
+/usr/local/chkrootkit/chkrootkit
+) | /bin/mail -s 'CHROOTKIT Daily Run' $ALERT_EMAIL
+EOM
+chmod 700 /etc/cron.daily/chkrootkit.sh
+
+
+
+echo "--- Installing Root Kit Hunter ---"
+echo "----------------------------------"
+wget -O /usr/local/src/rkhunter.tar.gz "https://sourceforge.net/projects/rkhunter/files/latest/download"
+tar -C /usr/local/src/ -zxvf /usr/local/src/rkhunter.tar.gz
+cd /usr/local/src/rkhunter*
+./installer.sh --layout default --install
+/usr/local/bin/rkhunter --update
+/usr/local/bin/rkhunter --propupd
+rm -Rf /usr/local/src/rkhunter*
+/bin/cat << EOM > /etc/cron.daily/rkhunter.sh
+#!/bin/sh
+(
+/usr/local/bin/rkhunter --versioncheck
+/usr/local/bin/rkhunter --update
+/usr/local/bin/rkhunter --cronjob --report-warnings-only
+) | /bin/mail -s 'rkhunter Daily Run' $ALERT_EMAIL
+EOM
+chmod 700 /etc/cron.daily/rkhunter.sh
+
+
+
+echo "--- Installing LSM (Linux Socket Monitor) ---"
+echo "---------------------------------------------"
+wget -O /usr/local/src/lsm-current.tar.gz http://www.rfxn.com/downloads/lsm-current.tar.gz
+tar -C /usr/local/src/ -zxvf /usr/local/src/lsm-current.tar.gz
+cd /usr/local/src/lsm-0.*
+sh install.sh
+cd /root
+rm -Rf /usr/local/src/lsm-*
+sed -i 's/root/'$ALERT_EMAIL/ /usr/local/lsm/conf.lsm
+/usr/local/sbin/lsm -g
+
+
+echo "--- Making changes to /etc/sysctl.conf ---"
+echo "------------------------------------------"
+echo 'net.ipv4.tcp_syncookies = 1' >> /etc/sysctl.conf
+echo 'net.ipv4.tcp_synack_retries = 2' >> /etc/sysctl.conf
+echo 'net.ipv4.conf.all.rp_filter = 1' >> /etc/sysctl.conf
+echo 'net.ipv4.conf.default.rp_filter = 1' >> /etc/sysctl.conf
+echo 'net.ipv4.conf.all.accept_redirects = 0' >> /etc/sysctl.conf
+echo 'net.ipv4.conf.all.secure_redirects = 0' >> /etc/sysctl.conf
+echo 'net.ipv4.conf.all.accept_source_route = 0' >> /etc/sysctl.conf
+echo 'net.ipv4.conf.all.send_redirects = 0' >> /etc/sysctl.conf
+echo 'net.ipv4.conf.default.send_redirects = 0' >> /etc/sysctl.conf
+
+/sbin/sysctl net.ipv4.tcp_syncookies=1
+/sbin/sysctl net.ipv4.tcp_synack_retries=2
+/sbin/sysctl net.ipv4.conf.all.rp_filter=1
+/sbin/sysctl net.ipv4.conf.default.rp_filter=1
+/sbin/sysctl net.ipv4.conf.all.accept_redirects=0
+/sbin/sysctl net.ipv4.conf.all.secure_redirects=0
+/sbin/sysctl net.ipv4.conf.all.accept_source_route=0
+/sbin/sysctl net.ipv4.conf.all.send_redirects=0
+/sbin/sysctl net.ipv4.conf.default.send_redirects=0
+
+
+
+echo "--- Modify SELinux to allow sshd to listen to new SSH port ---"
+echo "--------------------------------------------------------------"
+# Install semanage
+yum install -y policycoreutils-python
+# Allow SSH port
+semanage port -a -t ssh_port_t -p tcp $SSH_PORT
+
+
+
+echo "--- Copy root's SSH authorized keys to new user ---"
+echo "-----------------------------------------------------"
+mkdir /home/admin/.ssh
+chmod 700 /home/admin/.ssh
+cp /root/.ssh/authorized_keys /home/admin/.ssh/authorized_keys
+chmod 600 /home/admin/.ssh/authorized_keys
+chown -R admin:admin /home/admin/.ssh
+
+
+
+echo "--- Securing the SSH Daemon ---"
+echo "-------------------------------"
+echo "Backing up previous SSHd configurations"
+mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+/bin/cat << EOM > /etc/ssh/sshd_config
+## Random SSH port
+Port $SSH_PORT
+
+## Sets listening address on server. default=0.0.0.0
+#ListenAddress 192.168.0.1
+
+## Enforcing SSH Protocol 2 only
+Protocol 2
+
+## Disable direct root login, with no you need to login with admin user, then "su -" you into root
+PermitRootLogin no
+
+# Authentication
+LoginGraceTime 1m
+
+# Disable password login
+PermitEmptyPasswords no
+PasswordAuthentication no
+
+# Enable 2FA
+ChallengeResponseAuthentication yes
+UsePAM yes
+AuthenticationMethods publickey,password publickey,keyboard-interactive
+
+##
+UsePrivilegeSeparation yes
+
+##
+AllowTcpForwarding yes
+
+## Disables X11Forwarding
+X11Forwarding no
+
+## Checks users on their home directority and rhosts, that they arent world-writable
+StrictModes yes
+
+## The option IgnoreRhosts specifies whether rhosts or shosts files should not be used in authentication
+IgnoreRhosts yes
+
+##
+HostbasedAuthentication no
+
+## RhostsAuthentication specifies whether sshd can try to use rhosts based authentication.
+RhostsRSAAuthentication no
+
+## Enable / Disable sftp server
+Subsystem      sftp    /usr/libexec/openssh/sftp-server
+
+## Add users that are allowed to log in
+AllowUsers admin
+EOM
+
+
+
+echo "--- Install VIM ---"
+echo "-------------------"
+# Installing latest vim
+yum -y install vim-X11 vim-common vim-enhanced vim-minimal
+# Create .vimrc file for admin user
+/bin/cat << EOM > /home/admin/.vimrc
+set history=700
+
+colo delek
+syntax on
+
+set showmode
+
+set autoindent
+set smartindent
+set backspace=eol,start,indent
+
+set expandtab
+set tabstop=4
+set shiftwidth=4
+set ruler
+
+set number
+set ignorecase
+set smartcase
+set hlsearch
+set backup
+set backupdir=~/vim/tmp/
+set nowrap
+set laststatus=2
+set cmdheight=2
+EOM
+
+
+
+echo "******************************************"
+echo "     YOUR SERVER IS NOW BOOTSTRAPPED"
+echo "------------------------------------------"
+echo "SSH User: admin"
+echo "SSH Port: $SSH_PORT"
+echo "Alert Email: $ALERT_EMAIL"
+echo "******************************************************************"
+echo ""
+echo "You must now reconnect to this server using the information above."
+echo "Changing the SSH port has caused this connection to freeze."
+echo "BEFORE CLOSING THIS WINDOW please note your information above."
+echo "------------------------------------------------------------------"
+/sbin/service sshd restart
